e5e1142696b646f09e9e2c9eb9fd0d5cf3ef2359
trevorlink <lvpidadiao@hotmail.com>
2022-08-10 01:15:56 +0800

    add luminance switch

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::time::{Duration, Instant};
+use crate::canvas::Canvas;
+use crate::shapes::donut::Donut;
+
 mod shapes;
 mod canvas;
 
@@ -5,7 +9,21 @@ fn main() {
     print!("\x1b[2J"); // clear screen
     print!("\x1b[?25l");// hide terminal cursor
 
+    let canvas = Canvas::new(50, 50);
+    let mut torus = Donut::new(10, 5, 15, 25, 50, 50);
 
+
+    let sleep_duration = 1000 / 30; // fps 30
+    let start_ts = Instant::now();
+
+    loop {
+        canvas.buffer_2_screen_raw(&torus.next_frame_with_xy_rotate(true));
+        std::thread::sleep(Duration::from_millis(sleep_duration));
+
+        if start_ts.elapsed().as_secs() >= 10 {
+            break;
+        }
+    }
 
     print!("\x1b[?25h");  // show cursor
 }
--- src/shapes/donut.rs
+++ src/shapes/donut.rs
@@ -1,9 +1,7 @@
-use std::f32::consts::PI;
-use std::mem::size_of;
-use std::ops::Rem;
+use std::f32::consts::{TAU};
 
-pub static LUMINANCE_CHARS: [char; 12] = ['.', ',', '-','~',':', ';', '=', '!', '*', '#', '$', '@'];
-static RESO:f32 = 0.05;
+pub static LUMINANCE_CHARS: [char; 12] = ['.', ',', '-', '~', ':', ';', '=', '!', '*', '#', '$', '@'];
+static RESO: f32 = 0.05;
 
 
 pub struct Donut {
@@ -13,11 +11,9 @@ pub struct Donut {
     object_distance: u32,
     scr_width: usize,
     scr_height: usize,
-    psi: f32,
     delta: f32,
     a_radian: f32,
-    b_radian: f32,
-    light_source: [f32;3]
+    light_source: [f32; 3],
 }
 
 
@@ -30,23 +26,20 @@ impl Donut {
             object_distance: od,
             scr_width: sw,
             scr_height: sl,
-            psi: 0.0,
             delta: 0.0,
             a_radian: 0.0,
-            b_radian: 0.0,
             light_source: [1.0, 0.0, 1.0],
         }
     }
 
     // zx平面画圆，围绕z轴转动
     // r2*cos(psi)*cos(the) + r1*cos(psi), -r2*sin(psi)*cos(the) - r1*sin(psi), r2*sin(the)
+    #[allow(dead_code)]
     pub fn regulated_pixels(&self) -> Vec<Vec<char>> {
-        let mut zbuf = vec![vec![-123;self.scr_height];self.scr_width];
-        let mut output = vec![vec!['\0';self.scr_height];self.scr_width];
+        let mut zbuf = vec![vec![-123; self.scr_height]; self.scr_width];
+        let mut output = vec![vec!['\0'; self.scr_height]; self.scr_width];
         let mut psi = 0.0f32;
         let mut theta = 0.0f32;
-        let mut delta = 0.0f32;
-        let two_pi = PI * 2.0;
 
         let wid_offset = (self.scr_width / 2) as f32;
         let hei_offset = (self.scr_height / 2) as f32;
@@ -73,12 +66,12 @@ impl Donut {
                     output[xp][yp] = '*';
                 }
 
-                if theta.ge(&two_pi) {
+                if theta.ge(&TAU) {
                     break;
                 }
             }
             theta = 0.0;
-            if psi.ge(&two_pi) {
+            if psi.ge(&TAU) {
                 break;
             }
         }
@@ -88,23 +81,23 @@ impl Donut {
 
     // torus rotate along with y-axis counter-clockwise
     // [-r2*sin(delta)*sin(the) + r2*cos(delta)*cos(psi)*cos(the) + r1*cos(delta)*cos(psi), -r2*sin(psi)*cos(the) - r1*sin(psi), r2*sin(delta)*cos(psi)*cos(the) + r1*sin(delta)*cos(psi) + r2*sin(the)*cos(delta)]
+    #[allow(dead_code)]
     pub fn next_frame(&mut self) -> Vec<Vec<char>> {
-        let mut zbuf = vec![vec![-123;self.scr_height];self.scr_width];
-        let mut output = vec![vec!['\0';self.scr_height];self.scr_width];
+        let mut zbuf = vec![vec![-123; self.scr_height]; self.scr_width];
+        let mut output = vec![vec!['\0'; self.scr_height]; self.scr_width];
         let mut psi = 0.0f32;
         let mut theta = 0.0f32;
-        let two_pi = PI * 2.0;
 
         let wid_offset = (self.scr_width / 2) as f32;
         let hei_offset = (self.scr_height / 2) as f32;
 
-        self.delta += 0.17;
+        self.delta += RESO;
         let (sin_d, cos_d) = self.delta.sin_cos();
 
         loop {
-            psi += 0.05;
+            psi += RESO;
             loop {
-                theta += 0.05;
+                theta += RESO;
                 let (sin_t, cos_t) = theta.sin_cos();
                 let (sin_p, cos_p) = psi.sin_cos();
                 let z = (self.r2 as f32 * sin_d * cos_p * cos_t + self.r1 as f32 * sin_d * cos_p + self.r2 as f32 * sin_t * cos_d) as i32;
@@ -122,12 +115,12 @@ impl Donut {
                     output[xp][yp] = '*';
                 }
 
-                if theta.ge(&two_pi) {
+                if theta.ge(&TAU) {
                     break;
                 }
             }
             theta = 0.0;
-            if psi.ge(&two_pi) {
+            if psi.ge(&TAU) {
                 break;
             }
         }
@@ -145,12 +138,11 @@ impl Donut {
     // -sin(delta)*sin(the) + cos(delta)*cos(psi)*cos(the),
     // (sin(delta)*cos(psi)*cos(the) + sin(the)*cos(delta))*sin(a) - sin(psi)*cos(a)*cos(the)
     // (sin(delta)*cos(psi)*cos(the) + sin(the)*cos(delta))*cos(a) + sin(a)*sin(psi)*cos(the)
-    pub fn next_frame_with_x_rotate(&mut self) -> Vec<Vec<char>> {
-        let mut zbuf = vec![vec![-123;self.scr_height];self.scr_width];
-        let mut output = vec![vec!['\0';self.scr_height];self.scr_width];
+    pub fn next_frame_with_xy_rotate(&mut self, light: bool) -> Vec<Vec<char>> {
+        let mut zbuf = vec![vec![-123; self.scr_height]; self.scr_width];
+        let mut output = vec![vec!['\0'; self.scr_height]; self.scr_width];
         let mut psi = 0.0f32;
         let mut theta = 0.0f32;
-        let two_pi = PI * 2.0;
 
         let wid_offset = (self.scr_width / 2) as f32;
         let hei_offset = (self.scr_height / 2) as f32;
@@ -158,21 +150,21 @@ impl Donut {
         self.delta += RESO;
         self.a_radian += RESO;
         let (sin_d, cos_d) = self.delta.sin_cos();
-        let(sin_a, cos_a) = self.a_radian.sin_cos();
+        let (sin_a, cos_a) = self.a_radian.sin_cos();
 
         loop {
-            psi += 0.05;
+            psi += RESO;
             loop {
-                theta += 0.05;
+                theta += RESO;
                 let (sin_t, cos_t) = theta.sin_cos();
                 let (sin_p, cos_p) = psi.sin_cos();
-                let z = ((self.r2 * cos_t + self.r1)* sin_d * cos_p + self.r2 * sin_t * cos_d) * cos_a +
+                let z = ((self.r2 * cos_t + self.r1) * sin_d * cos_p + self.r2 * sin_t * cos_d) * cos_a +
                     (self.r2 * cos_t + self.r1) * sin_a * sin_p;
                 let z_apo = self.viewer_distance as f32 / (self.object_distance as f32 - z);
 
 
-                let x = (self.r2 * cos_t + self.r1)* cos_d * cos_p - self.r2 * sin_d * sin_t;
-                let y = ((self.r2 * cos_t + self.r1)* sin_d * cos_p + self.r2 * sin_t * cos_d)*sin_a -
+                let x = (self.r2 * cos_t + self.r1) * cos_d * cos_p - self.r2 * sin_d * sin_t;
+                let y = ((self.r2 * cos_t + self.r1) * sin_d * cos_p + self.r2 * sin_t * cos_d) * sin_a -
                     (self.r2 * cos_t + self.r1) * sin_p * cos_a;
 
 
@@ -180,36 +172,39 @@ impl Donut {
                 let yp = (y * z_apo + hei_offset) as usize;
 
 
-                if z as i32 > zbuf[xp][yp]  {
+                if z as i32 > zbuf[xp][yp] {
                     zbuf[xp][yp] = z as i32;
-                    let normal = [-1.0 * sin_d * sin_t + cos_d * cos_p * cos_t,
-                        (sin_d * cos_p * cos_t + sin_t * cos_d) * sin_a - sin_p * cos_a * cos_t,
-                        (sin_d * cos_p * cos_t + sin_t * cos_d) * cos_a + sin_a * sin_p * cos_t];
+                    if !light {
+                        output[xp][yp] = '*';
+                    } else {
+                        let normal = [-1.0 * sin_d * sin_t + cos_d * cos_p * cos_t,
+                            (sin_d * cos_p * cos_t + sin_t * cos_d) * sin_a - sin_p * cos_a * cos_t,
+                            (sin_d * cos_p * cos_t + sin_t * cos_d) * cos_a + sin_a * sin_p * cos_t];
 
-                    let lumi =  dot_product_3d(normal, &self.light_source);
+                        let lumi = Donut::dot_product_3d(normal, &self.light_source);
 
-                    if lumi < 0.0 {
-                        continue;
+                        if lumi < 0.0 {
+                            output[xp][yp] = LUMINANCE_CHARS[0];
+                        } else {
+                            let lumi_index = (lumi * 6.5) as usize;
+                            output[xp][yp] = LUMINANCE_CHARS[lumi_index];
+                        }
                     }
-                    //
-                    let lumi_index = (lumi * 6.5) as usize;
-                    output[xp][yp] = LUMINANCE_CHARS[lumi_index];
                 }
 
-                if theta.ge(&two_pi) {
+                if theta.ge(&TAU) {
                     break;
                 }
             }
             theta = 0.0;
-            if psi.ge(&two_pi) {
+            if psi.ge(&TAU) {
                 break;
             }
         }
         return output;
     }
 
-    pub fn dot_product_3d(a: [f32;3], b: &[f32;3]) -> f32 {
+    pub fn dot_product_3d(a: [f32; 3], b: &[f32; 3]) -> f32 {
         a[0] * b[0] + a[1] * b[1] + a[2] * b[2]
     }
-
 }
